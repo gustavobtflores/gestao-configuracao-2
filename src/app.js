363e7c919a2aa81890acabc97a92c36e77600346
Gustavo Flores <gustavobtflores@gmail.com>
2025-12-10 19:32:37 -0300

    refactor: rename files and route to use english
    Signed-off-by: Gustavo Flores <gustavobtflores@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { filmesRouter } from "./controllers/routes/filmes.js";
+import { moviesRouter } from "./controllers/routes/movies.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(filmesRouter);
+app.use(moviesRouter);
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
